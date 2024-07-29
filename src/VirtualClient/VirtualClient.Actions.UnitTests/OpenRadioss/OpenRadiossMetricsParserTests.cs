1764cd8e1d148afac73209b197eb881523adf58e
ShobhaKumari07 <shobhakumari@microsoft.com>
2024-07-29 10:50:22 +0530

    openradioss

---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/OpenRadioss/OpenRadiossMetricsParserTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualClient.Common.Contracts;
+using NUnit.Framework;
+using VirtualClient.Contracts;
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    [TestFixture]
+    [Category("Unit")]
+    class OpenRadiossMetricsParserTests
+    {
+        [TestFixture]
+        [Category("Unit")]
+        public class OpenRadiossRuntimeParserTests
+        {
+            private string rawText;
+            private OpenRadiossMetricsParser testParser;
+
+            [SetUp]
+            public void Setup()
+            {
+                string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string outputPath = Path.Combine(workingDirectory, "Examples", "OpenRadioss", "OpenRadiossResultsExample.out");
+                this.rawText = File.ReadAllText(outputPath);
+                this.testParser = new OpenRadiossMetricsParser(this.rawText);
+            }
+
+            [Test]
+            public void OpenRadiossParserVerifyMetrics()
+            {
+                IList<Metric> metrics = this.testParser.Parse();
+
+                Assert.AreEqual(3, metrics.Count);
+                MetricAssert.Exists(metrics, "StarterEngineRuntime", 66370.16);
+                MetricAssert.Exists(metrics, "TotalNumberOfCycles", 160039);
+                MetricAssert.Exists(metrics, "NumberOfCyclesPerMinute", 144);
+            }
+
+            [Test]
+            public void OpenRadiossParserThrowIfInvalidOutputFormat()
+            {
+                string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string incorrectOutputPath = Path.Combine(workingDirectory, "Examples", "OpenRadioss", "OpenRadiossResultsInvalidExample.out");
+                this.rawText = File.ReadAllText(incorrectOutputPath);
+                this.testParser = new OpenRadiossMetricsParser(this.rawText);
+
+                SchemaException exception = Assert.Throws<SchemaException>(() => this.testParser.Parse());
+                StringAssert.Contains("Input text is null or empty.", exception.Message);
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions/OpenRadioss/OpenRadiossMetricsParser.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text.RegularExpressions;
+    using VirtualClient.Common.Contracts;
+    using VirtualClient.Contracts;
+    using DataTableExtensions = VirtualClient.Contracts.DataTableExtensions;
+
+    /// <summary>
+    /// Parser for OpenRadioss output document.
+    /// </summary>
+    public class OpenRadiossMetricsParser : MetricsParser
+    {
+        /// <summary>
+        /// Separate the column values by 2 or more spaces.
+        /// </summary> 
+        private static readonly string StarterEngineRuntimePattern = @"STARTER\+ENGINE RUNTIME =\s+([\d\.]+)s \(([\d:]+)\)";
+
+        /// <summary>
+        /// Separate the column values by 2 or more spaces.
+        /// </summary> 
+        private static readonly string TotalNumberOfCyclesPattern = @"TOTAL NUMBER OF CYCLES\s+:\s+(\d+)";
+
+        // private static readonly Regex OpenRadiossSectionDelimiter = new Regex(@"(\n)(\s)*(\n)", RegexOptions.ExplicitCapture);
+
+        /// <summary>
+        /// constructor for <see cref="OpenRadiossMetricsParser"/>.
+        /// </summary>
+        /// <param name="rawText">Raw text to parse.</param>
+        public OpenRadiossMetricsParser(string rawText)
+            : base(rawText)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override IList<Metric> Parse()
+        {
+            this.ThrowIfInvalidOutputFormat();
+            List<Metric> metrics = new List<Metric>();
+
+            string input = this.RawText;
+
+            // Parse STARTER+ENGINE RUNTIME
+            var starterEngineMatch = Regex.Match(input, StarterEngineRuntimePattern);
+            if (starterEngineMatch.Success)
+            {
+                string runtimeSecondsStr = starterEngineMatch.Groups[1].Value;
+                double runtimeSeconds = double.Parse(runtimeSecondsStr);
+                metrics.Add(new Metric("StarterEngineRuntime", runtimeSeconds, "seconds"));
+            }
+            else
+            {
+                throw new SchemaException("Unable to parse STARTER+ENGINE RUNTIME.");
+            }
+
+            // Parse TOTAL NUMBER OF CYCLES
+            var totalCyclesMatch = Regex.Match(input, TotalNumberOfCyclesPattern);
+            if (totalCyclesMatch.Success)
+            {
+                int totalCycles = int.Parse(totalCyclesMatch.Groups[1].Value);
+                metrics.Add(new Metric("TotalNumberOfCycles", totalCycles, "Cycles"));
+            }
+            else
+            {
+                throw new SchemaException("Unable to parse TOTAL NUMBER OF CYCLES.");
+            }
+
+            // Parse TOTAL NUMBER OF CYCLES
+            if (starterEngineMatch.Success && totalCyclesMatch.Success)
+            {
+                string runtimeSecondsStr = starterEngineMatch.Groups[1].Value;
+                double runtimeSeconds = double.Parse(runtimeSecondsStr);
+                int totalCycles = int.Parse(totalCyclesMatch.Groups[1].Value);
+                double t = (totalCycles * 60) / (runtimeSeconds);
+                int x = (int)t;
+                metrics.Add(new Metric("NumberOfCyclesPerMinute", x, "Cycles/min"));
+
+            }
+
+            return metrics;
+        }
+
+        /// <inheritdoc/>
+        private void ThrowIfInvalidOutputFormat()
+        {
+            if (string.IsNullOrEmpty(this.RawText))
+            {
+                throw new SchemaException("Input text is null or empty.");
+            }
+        }
+    }
+
+}
